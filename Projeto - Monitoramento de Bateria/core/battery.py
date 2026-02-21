c37e50863e56f5e2c1ea4fd324fa7d3dc83e8aba
douglasmasuzzo <dev.masdouglas4001@gmail.com>
2026-02-21 19:01:51 -0300

    Redefinindo métricas de cálculo
    Recodificando o método de cálculo do tempo estimado da bateria

--- Projeto - Monitoramento de Bateria/core/battery.py
+++ Projeto - Monitoramento de Bateria/core/battery.py
@@ -1,20 +1,35 @@
 import psutil
 from datetime import datetime
 
-def status_bateria():
+def status_bateria( percent_ant=None, timestamp_ant=None ):
     
     bateria = psutil.sensors_battery()
-    if not bateria: 
+    if not bateria:
         return None
     
-    tempo = bateria.secsleft
+    percent_atual = bateria.percent
+    timestamp_atual = datetime.now()
 
-    if tempo < -2 :
-        tempo = -1
+    tempo_estimado = bateria.secsleft
 
-    return {
-        "timestamp": datetime.now(),
-        "percent": bateria.percent,
+    if tempo_estimado in (
+        psutil.POWER_TIME_UNLIMITED, psutil.POWER_TIME_UNKNOWN
+    ):
+        
+        tempo_estimado = - 1
+
+    if percent_ant is not None and timestamp_ant is not None :
+
+        percent = percent_ant - percent_atual
+        tempo = ( timestamp_atual - timestamp_ant ).total_seconds()
+
+        if percent > 0 and tempo > 0 :
+            descarga = percent / tempo 
+            tempo_estimado = int ( percent_atual / descarga )
+
+    return{
+        "timestamp": timestamp_atual,
+        "percent": percent_atual,
         "plugged": bateria.power_plugged,
-        "running_time": bateria.secsleft
+        "running_time": tempo_estimado
     }
